e1ace83956144e989c57ab91d31cd6a30586a8db
Manoj <manojd@rgmobiles.com>
2020-10-14 23:42:52 +0530

    heroku-mern-backen

---
+++ data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    url:
+      "https://assets.mixkit.co/videos/preview/mixkit-a-hand-browsing-on-a-smartphone-12112-large.mp4",
+    channel: "csd",
+    song: "waste of hate",
+    likes: "232",
+    messages: "222",
+    description: "this is an example decshbs",
+    shares: "22",
+  },
+
+  {
+    url:
+      "https://assets.mixkit.co/videos/preview/mixkit-a-hand-browsing-on-a-smartphone-12112-large.mp4",
+    channel: "csd",
+    song: "waste of hate",
+    likes: "232",
+    messages: "222",
+    description: "this is an example decshbs",
+    shares: "22",
+  },
+];
+
+export default data;
---
+++ server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import mongoose from "mongoose";
+import Data from "./data.js";
+import Videos from "./dbModel.js";
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+//middle wares
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-allow-Origin", "*"),
+    res.setHeader("Access-Control-allow-Headers", "*"),
+    next();
+});
+
+//DB config
+const connection_url =
+  "mongodb+srv://admin:admin@cluster0.ov98y.mongodb.net/tiktok?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+//api endpoints
+app.get("/", (req, res) => res.status(200).send("hello Heroku"));
+
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
+
+app.get("/v2/posts", (req, res) => {
+  Videos.find({}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/v2/posts", (req, res) => {
+  //post request is to Add DATA to the database
+  //It will let us Add a video document to the videos collection
+  const dbVideos = req.body;
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+//listen
+app.listen(port, () => console.log(`listening on localhost:${port}`));
